f77187aa532d1b7813b89c520a80118c88093ca6
Kaegan O'Hara <kohara321@gmail.com>
2022-06-09 15:20:35 -0600

    commit tree 80a681d8bede1a34ab8f6d175e7092a1d23fd39f

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,31 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [filter, setFilter] = useState('All')
+
+function handleFilter(category) {
+    setFilter(category)
+}
+  const newFilterArray = tasks.filter(task => {
+    if(filter === "All") {
+      return true
+    } else {
+      return(
+      task.category === filter
+   )}
+  })
+
+  function handleDelete(text) {
+    const deleteText = tasks.filter(task => task.text !== text)
+    setTasks(deleteText)
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter filter={filter} categories={CATEGORIES} handleFilter={handleFilter}/>
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={newFilterArray} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ filter, categories, handleFilter }) {
+  const categoriesArray = categories.map(category => <button key={category} className='' onClick={() => handleFilter(category)}>{category}</button>)
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoriesArray}
     </div>
   );
 }
